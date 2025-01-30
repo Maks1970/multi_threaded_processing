872c022297f410de0c3419b557ec12b10ff86513
Max Kosh <koshelmak@gmail.com>
2025-01-30 08:38:30 +0200

    Add project files.

---
+++ multi_threaded_processing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace multi_threaded_processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
