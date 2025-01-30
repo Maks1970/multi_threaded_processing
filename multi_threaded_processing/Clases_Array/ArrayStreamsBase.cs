95d955717e232becd0932c1e2f9e9039ed0d56dc
Max Kosh <koshelmak@gmail.com>
2025-01-30 12:36:28 +0200

    Array

---
+++ multi_threaded_processing/Clases_Array/ArrayStreamsBase.cs
@@ -0,0 +1,44 @@
+﻿
+namespace multi_threaded_processing
+{
+    public class ArrayStreamsBase
+    {
+        protected readonly int[] arr;
+        protected readonly int threadCount;
+        protected Thread[] _threads = Array.Empty<Thread>();
+        public ArrayStreamsBase(int threadCount, int[] arr)
+        {
+            this.threadCount = threadCount;
+            this.arr = arr;
+        }
+        public int [] Run(Func<int,int,int> Process,int threadCount)
+        {
+            this._threads = new Thread[threadCount];
+            int chunkSize = arr.Length / threadCount;
+            int [] results = new int[threadCount];
+            // Створення потоків
+            for (int i = 0; i < threadCount; i++)
+            {
+                int startIndex = i * chunkSize;
+                int endIndex = (i + 1) * chunkSize;
+                if (i == threadCount - 1) endIndex = arr.Length; // Останній потік обробляє залишок
+
+                int[] subArray = new int[endIndex - startIndex];
+                Array.Copy(arr, startIndex, subArray, 0, subArray.Length);
+
+                var num = i;
+                _threads[i] = new Thread(() => results[num] = Process(startIndex, endIndex))
+                {
+                    IsBackground = true
+                };
+            }
+            foreach (var thread in _threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in _threads) { thread.Join(); }
+            return results;
+        }
+    }
+}
--- multi_threaded_processing/Program.cs
+++ multi_threaded_processing/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace multi_threaded_processing
+﻿using System.Diagnostics;
+
+namespace multi_threaded_processing
 {
-    internal class Program
+
+
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var arr = new int[20_000_000];//{ 1, 2, 2, 3, 4, 5, 6, 7, 8, 1};
+            var p = new RandomArrayThreadProcessor(4, arr);
+            var sw = Stopwatch.StartNew();
+            p.Run();
+            sw.Stop();
+            var procArr = new ArrayProcesing(4, arr);
+            Console.WriteLine($"Min = {procArr.Min(2)}");
+            Console.WriteLine($"Max = {procArr.Max(2)}");
+            Console.WriteLine($"Sum = {procArr.Sum(3)}");
+            Console.WriteLine($"Avarage = {procArr.Avarage(1)}");
+            var d = procArr.InCopyPart(3,2,8);
+            //Console.WriteLine(sw.Elapsed);
         }
     }
 }
